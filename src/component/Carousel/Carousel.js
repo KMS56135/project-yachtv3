296a488c1f8ec63a627d3a249b4a36c56c5c11c4
Yaroslav Dudar <y.dudar@list.ru>
2024-03-28 11:57:49 +0300

    28.03.2024

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -52,11 +52,12 @@ const Carousel = () => {
             </div>
           </div>
         ))}
-      </div>
       <button className="prev" onClick={prevSlide}>&#10094;</button>
       <button className="next" onClick={nextSlide}>&#10095;</button>
+      </div>
     </div>
   );
 };
 
 export default Carousel;
+
